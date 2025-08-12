14a05ad464d052cd40374ca850a8fbfb35546fb4
mhiss <markus@hiss-online.de>
2025-08-12 13:37:04 +0200

    Update config.h
    Reverse 1

--- Inc/config.h
+++ Inc/config.h
@@ -82,7 +82,7 @@
 #define GEAR_RATIO 105 //dummy for testing
 #define SPEEDLIMIT 25
 #define PULSES_PER_REVOLUTION 1
-#define REVERSE -1
+#define REVERSE 1  //-1
 #define SPEEDFILTER 1
 #define SPEEDSOURCE INTERNAL
 
